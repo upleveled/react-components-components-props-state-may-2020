c9682f05335a6ac74907c8994547ef721639cca3
Karl Horky <karl.horky@gmail.com>
2020-05-14 17:35:31 +0200

    Move Board out into another file

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,45 +1,5 @@
 import React, { useState } from 'react';
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
+import { Board } from './Board';
 
 export default function TicTacToe(props) {
   const [stepNumber, setStepNumber] = useState(0);
